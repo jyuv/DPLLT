324f97d678e8bbd0772f01612e1b3a6ebcdbbc67
Yuval <yuval,jacoby1@mail.huji.ac.il>
2022-03-13 10:12:18 +0200

    replaced all test to use pytest parametrize and parser. Added more tests

--- tests/test_logical_blocks.py
+++ tests/test_logical_blocks.py
@@ -1,84 +1,112 @@
-from logical_blocks import Var, Negate, Imply, Equiv, And, Or
+import pytest
+from parse import Parser
 
-x1 = Var("X1")
-x2 = Var("X2")
-x3 = Var("X3")
-x4 = Var("X4")
-
-f1 = Negate(Imply(x1, And(x1, x2)))  # (x1 -> (x1 & x2))
-f2 = Negate(Equiv(x1, And(x1, x2)))  # (x1 <-> (x1 & x2))
-f3 = And(Or(x1, Negate(x2)), Imply(x3, x2))  # (x1 || -x2) & (x3 -> x2)
+parser = Parser()
 
 
-def test_is_literal():
-    atoms = [x1, Negate(x1), Negate(Negate(x1)), Imply(Negate(x1), x2),
-             Equiv(x2, x3), And(x2, x3), Or(And(x1, x2), x3)]
-    atoms_is_literals = [a.is_literal() for a in atoms]
-    assert atoms_is_literals == [True, True, False, False, False, False, False]
+is_literal_params = [
+    ("x1", True),
+    ("!x1", True),
+    ("!!x1", False),
+    ("(!x1) -> x2", False),
+    ("x2 <-> x3", False),
+    ("x2 & x3", False),
+    ("x1 | x2", False),
+    ("(x1 & x2) | x3", False)
+]
 
 
-def test_print():
-    expected_1 = "!(X1->(X1&X2))"
-    expected_2 = "!(X1<->(X1&X2))"
-    expected_3 = "((X1|!X2)&(X3->X2))"
-    assert str(f1).replace(" ", "") == expected_1
-    assert str(f2).replace(" ", "") == expected_2
-    assert str(f3).replace(" ", "") == expected_3
+@pytest.mark.parametrize("formula_text, bool_is_literal",
+                         is_literal_params,
+                         ids=[repr(is_lit[0]) for is_lit in is_literal_params])
+def test_is_literal(formula_text, bool_is_literal):
+    formula = parser.parse(formula_text)
+    assert formula.is_literal() == bool_is_literal
 
 
-def test_negations():
-    assert Negate(x1).negate() == x1
-    assert Negate(Negate(x2)).negate() == Negate(x2)
+# Todo: add here =, !=,  >=, <, functions, double negations,
+#  double negations as args.
+@pytest.mark.parametrize("formula_text", [
+    "!(X1->(X1&X2))",
+    "!(X1<->(X1&X2))",
+    "(X1|!X2)&(X3->X2)"
+    "a = b",
+    "a = (c = d)",
+    "a != b",
+    "a != (c & d)",
+    "[1, 2] < 3",
+    "!([1, 2] < 3)",
+    "[1, 2] >= 3",
+    "!([1, 2] >= 3)",
+    "f(a, b)",
+    "f(a, b & c)",
+    "f(a = b, c | (d & s))"
+    "!!x",
+    "!!(a = b)",
+    "!!!x",
+    "!!!(a = b)"
+])
+def test_str(formula_text):
+    formula = parser.parse(formula_text)
+    assert str(formula).replace(" ", "") == formula_text.replace(" ", "")
 
-    assert And(x1, x2).negate() == Or(Negate(x1), Negate(x2))
-    assert And(Negate(x1), x2).negate() == Or(Negate(Negate(x1)), Negate(x2))
 
-    assert Or(x1, x2).negate() == And(Negate(x1), Negate(x2))
-    assert Or(Negate(Negate(x1)), Negate(x2)).negate() == And(Negate(Negate(Negate(x1))),
-                                                              Negate(Negate(x2)))
+# Todo: add __eq__ & __hash__ tests
+#  Make sure equalities with isinstance(type(self)) works
 
-
-def test_to_basic():
-    assert Imply(x1, x2).to_basic() == Or(Negate(x1), x2)
-    assert Imply(Negate(x1), Negate(x2)).to_basic() == Or(Negate(Negate(x1)), Negate(x2))
-    assert Imply(And(x1, x2), x3).to_basic() == Or(Negate(And(x1, x2)), x3)
-
-    assert Equiv(x1, x2).to_basic() == And(Or(Negate(x1), x2), Or(Negate(x2), x1))
-    assert Equiv(Negate(x1), Negate(x2)).to_basic() == And(Or(Negate(Negate(x1)), Negate(x2)),
-                                                     Or(Negate(Negate(x2)), Negate(x1)))
-    assert Equiv(And(x1, x2), x3).to_basic() == And(Or(Negate(And(x1, x2)), x3),
-                                                    Or(Negate(x3), And(x1, x2)))
+# Todo: add here =, !=, >=, <, functions
+negations_params = [
+    ("!x1", "x1"),
+    ("!!x2", "!x2"),
+    ("x1 & x2", "(!x1) | !x2"),
+    ("(!x1) & x2", "(!!x1) | !x2"),
+    ("x1 | x2", "(!x1) & !x2"),
+    ("(!!x1) | !x2", "(!!!x1) & !!x2")
+]
 
 
-def test_cnf_distribute():
-    assert Or(x1, x2).cnf_distribute() == Or(x1, x2)
-    assert Or(Negate(x1), Negate(x2)).cnf_distribute() == Or(Negate(x1), Negate(x2))
-    assert Or(Negate(Negate(x1)), Negate(x2)).cnf_distribute() == Or(Negate(Negate(x1)),
-                                                            Negate(x2))
+@pytest.mark.parametrize("formula_text, expected_negation_text",
+                         negations_params,
+                         ids=[neg_param[0] for neg_param in negations_params])
+def test_negations(formula_text, expected_negation_text):
+    formula = parser.parse(formula_text)
+    expected_negation = parser.parse(expected_negation_text)
+    assert formula.negate() == expected_negation
 
-    assert Or(And(x1, x2), x3).cnf_distribute() == And(Or(x1, x3), Or(x2, x3))
-    assert Or(And(x1, x2), Negate(x3)).cnf_distribute() == And(Or(x1, Negate(x3)),
-                                                            Or(x2, Negate(x3)))
 
-    assert Or(x1, And(x2, x3)).cnf_distribute() == And(Or(x1, x2), Or(x1, x3))
-    assert Or(Negate(x1), And(x2, x3)).cnf_distribute() == And(Or(Negate(x1), x2),
-                                                            Or(Negate(x1), x3))
+to_basic_params = [
+    ("x1 -> x2", "(!x1) | x2"),
+    ("(!x1) -> !x2", "(!!x1) | !x2"),
+    ("(x1 & x2) -> x3", "(!(x1 & x2)) | x3"),
+    ("x1 <-> x2", "((!x1) | x2) & ((!x2) | x1)"),
+    ("(!x1) <-> !x2", "((!!x1) | !x2) & ((!!x2) | !x1)"),
+    ("(x1 & x2) <-> x3", "((!(x1 & x2)) | x3) & ((!x3) | (x1 & x2))")
+]
 
-    assert Or(Negate(And(x1, x2)), Negate(x3)).cnf_distribute() == Or(Negate(And(x1,
-                                                                        x2)),
-                                                                Negate(x3))
-    assert Or(Negate(x3), Negate(And(x1, x2))).cnf_distribute() == Or(Negate(x3),
-                                                                Negate(And(x1,
-                                                                        x2)))
 
-    assert Or(And(x1, x2), And(x3, x4)).cnf_distribute() == And(Or(x1, x3),
-                                                                And(
-                                                                    Or(x1, x4),
-                                                                    And(
-                                                                        Or(x2,
-                                                                           x3),
-                                                                        Or(x2,
-                                                                           x4)
-                                                                        )
-                                                                    )
-                                                                )
+@pytest.mark.parametrize("formula_text, basic_form_text",
+                         to_basic_params,
+                         ids=[tb_param[0] for tb_param in to_basic_params])
+def test_to_basic(formula_text, basic_form_text):
+    formula = parser.parse(formula_text)
+    basic_form = parser.parse(basic_form_text)
+    assert formula.to_basic() == basic_form
+
+
+@pytest.mark.parametrize("formula_text, cnf_distribute_text", [
+    ("x1 | x2", "x1 | x2"),
+    ("(!x1) | (!x2)", "(!x1) | (!x2)"),
+    ("(!!x1) | (!x2)", "(!!x1) | (!x2)"),
+    ("(x1 & x2) | x3", "(x1 | x3) &  (x2 | x3)"),
+    ("(x1 & x2) | (!x3)", "(x1 | !x3) & (x2 | !x3)"),
+    ("x1 | (x2 & x3)", "(x1 | x2) & (x1 | x3)"),
+    ("(!x1) | (x2 & x3)", "((!x1) | x2) & ((!x1) | x3)"),
+    ("(!(x1 & x2)) | (!x3)", "(!(x1 & x2)) | (!x3)"),
+    ("(!x3) | (!(x1 & x2))", "(!x3) | (!(x1 & x2))"),
+    ("(x1 & x2) | (x3 & x4)",
+     "(x1 | x3) & ((x1 | x4) & ((x2 | x3) & (x2 | x4)))")
+])
+def test_cnf_distribute(formula_text, cnf_distribute_text):
+    formula = parser.parse(formula_text)
+    cnf_distribute_form = parser.parse(cnf_distribute_text)
+    assert formula.cnf_distribute() == cnf_distribute_form
