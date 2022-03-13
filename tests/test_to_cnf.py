aca4858557d7b0dee75a432e2f352045563c2ef4
Yuval <yuval,jacoby1@mail.huji.ac.il>
2022-03-13 16:50:57 +0200

    split test_bool_transforms and parametrized using pytest. Added args negations tests for tseitin_transform

---
+++ tests/test_to_cnf.py
@@ -0,0 +1,42 @@
+import pytest
+from parse import Parser
+from bool_transforms.to_cnf import to_nnf, to_cnf
+
+parser = Parser()
+
+
+formulas_texts = ["!(x1 -> (x1 & x2))",
+                  "x1 <-> (x2 -> x3)",
+                  "(x1 & x2) | (x3 & x4)",
+                  "!((!(x1 & x2)) -> (!x3))"
+                  ]
+
+formulas_nnf_texts = ["x1 & ((!x1) | (!x2))",
+                      "((!x1) | ((!x2) | x3)) & ((x2 & (!x3)) | x1)",
+                      "(x1 & x2) | (x3 & x4)",
+                      "((!x1) | (!x2)) & x3"
+                      ]
+
+formulas_cnf_texts = ["x1 & ((!x1) | (!x2))",
+                      "((!x1) | ((!x2) | x3))  &  ((x2 | x1) & ((!x3) | x1))",
+                      "(x1 | x3)  &  ((x1 | x4) & ((x2 | x3) & (x2 | x4)))",
+                      "((!x1) | (!x2)) & x3"
+                      ]
+
+
+@pytest.mark.parametrize("formula_text, expected_nnf_text",
+                         zip(formulas_texts, formulas_nnf_texts),
+                         ids=formulas_texts)
+def test_to_nnf_correctness(formula_text, expected_nnf_text):
+    formula = parser.parse(formula_text)
+    expected_nnf = parser.parse(expected_nnf_text)
+    assert to_nnf(formula) == expected_nnf
+
+
+@pytest.mark.parametrize("formula_text, expected_cnf_text",
+                         zip(formulas_texts, formulas_cnf_texts),
+                         ids=formulas_texts)
+def test_to_cnf_correctness(formula_text, expected_cnf_text):
+    formula = parser.parse(formula_text)
+    expected_cnf = parser.parse(expected_cnf_text)
+    assert to_cnf(formula) == expected_cnf
---
+++ tests/test_tseitin_transform.py
@@ -0,0 +1,79 @@
+from bool_transforms.tseitin_transform import tseitin_transform
+from logical_blocks import Var, Or, And, Negate, Equiv, Imply, Func, NEqual
+import pytest
+from parse import Parser
+from bool_transforms.to_cnf import to_cnf
+
+parser = Parser()
+
+p = Var("p")
+q = Var("q")
+r = Var("r")
+
+g0 = Var("#G0")
+g1 = Var("#G1")
+g2 = Var("#G2")
+g3 = Var("#G3")
+
+n0 = Var("#N0")
+n1 = Var("#N1")
+
+neg_g0 = Negate(g0)
+neg_p = Negate(p)
+
+neq_r_n0 = NEqual(r, n0)
+neq_q_n0 = NEqual(q, n0)
+neq_q_n1 = NEqual(q, n1)
+neq_r_n1 = NEqual(r, n1)
+
+f_q_n0 = Func("f", [q, n0])
+f_f_q_n0_n0 = Func("f", [Func("f", [q, n0]), n0])
+f_n0_n1 = Func("f", [n0, n1])
+f_f_q_n0_n1 = Func("f", [Func("f", [q, n0]), n1])
+
+
+tseitin_formulas_texts = ["(p & q)  |  !(q | r)",
+                          "!((!(p & q)) -> !r)"
+                          ]
+
+tseitin_expected_equivs = [[g0, Equiv(g0, Or(g1, g2)), Equiv(g1, And(p, q)),
+                           Equiv(g2, Negate(g3)), Equiv(g3, Or(q, r))],
+
+                           [g0, Equiv(g0, Negate(g1)),
+                           Equiv(g1, Imply(g2, Negate(r))),
+                           Equiv(g2, Negate(g3)), Equiv(g3, And(p, q))]
+                           ]
+
+
+@pytest.mark.parametrize("formula_text, expected_equivs_ands",
+                         zip(tseitin_formulas_texts, tseitin_expected_equivs),
+                         ids=tseitin_formulas_texts)
+def test_tseitin_equivs(formula_text, expected_equivs_ands):
+    formula = parser.parse(formula_text)
+    expected_result = [to_cnf(equiv) for equiv in expected_equivs_ands]
+    assert tseitin_transform(formula) == expected_result
+
+
+args_negations_texts = ["p & f(q, !r)",
+                        "p & f(!q, !r)",
+                        "p & f(f(q, !r), !r)",
+                        "p & f(f(q, !r), !q)"
+                        ]
+
+negations_tseitins = [[g0, to_cnf(Equiv(g0, And(p, f_q_n0))), neq_r_n0],
+                      [g0, to_cnf(Equiv(g0, And(p, f_n0_n1))),
+                       neq_q_n0, neq_r_n1
+                       ],
+                      [g0, to_cnf(Equiv(g0, And(p, f_f_q_n0_n0))), neq_r_n0],
+                      [g0, to_cnf(Equiv(g0, And(p, f_f_q_n0_n1))),
+                       neq_r_n0, neq_q_n1
+                       ]
+                      ]
+
+
+@pytest.mark.parametrize("formula_text, expected_result",
+                         zip(args_negations_texts, negations_tseitins),
+                         ids=args_negations_texts)
+def test_tseitin_args_negations(formula_text, expected_result):
+    formula = parser.parse(formula_text)
+    assert tseitin_transform(formula) == expected_result
