621c653d67f59d6948e19a0554e3307082f3aa1b
Yuval <yuval,jacoby1@mail.huji.ac.il>
2022-03-19 11:44:30 +0200

    cleaned and arranged previously existed tests while adding test of UF preprocessing checks

--- tests/test_DPLLT_UFTheory.py
+++ tests/test_DPLLT_UFTheory.py
@@ -1,44 +1,47 @@
-from DPLLT import DPLLT
+import pytest
+
+from solvers.DPLLT import DPLLT
 from constants import ResultCode
-from parse import Parser
+from parsing.parse import Parser
 from theories.UFTheory import UFTheory
 
 
 str_uf1 = "(g(a) = c) & (((f(g(a)) != f(c)) | (g(a) = d)) & (c != d))"
 
-cls_strs_uf2 = ["a = b",
-                "(a != b) | ((s != t) | (b = c))",
-                "(s = t) | ((t != r) | (f(s) = f(a)))",
-                "(b != c) | ((t != r) | (f(s) = f(a)))",
-                "(f(s) != f(a)) | (f(a) != f(c))"]
-str_uf2 = "({0}) & (({1}) & (({2}) & (({3}) & ({4}))))".format(*cls_strs_uf2)
+clauses_strs_uf2 = ["a = b",
+                    "(a != b) | ((s != t) | (b = c))",
+                    "(s = t) | ((t != r) | (f(s) = f(a)))",
+                    "(b != c) | ((t != r) | (f(s) = f(a)))",
+                    "(f(s) != f(a)) | (f(a) != f(c))"]
+str_uf2 = "({0}) & (({1}) & (({2}) & (({3}) & ({4}))))".format(
+    *clauses_strs_uf2)
 
 str_uf3 = "(f(f(f(a))) = a) & ((f(f(f(f(f(a))))) = a) & (f(a) != a))"
 
-cls_strs_uf4 = ["(a = b) | (g(f(b)) != f(c))",
-                "(c != d) | ((h(h(h(a))) = h(a)) | (f(b) = h(p(a,b))))",
-                "(f(a) = c) | ((f(b) = f(f(g(h(d))))) | (a = c))",
-                "(e = r) | (s = a)",
-                "m(e, r) = m(a, b)",
-                "f(a) != g(h(m(a, r)))",
-                "m(e, r) = m(r, e)",
-                "(g(h(d)) != f(b)) | (g(h(m(r, a))) = f(b))",
-                "f(f(f(b))) = f(b)",
-                "g(h(m(r, a))) != h(p(a, b))",
-                "h(a) = e",
-                "h(e) = r",
-                "h(r) = b",
-                "e != b",
-                "p(a, b) = r",
-                "f(b) != f(f(b))",
-                "f(a) = c",
-                "f(b) = d"]
+clauses_strs_uf4 = ["(a = b) | (g(f(b)) != f(c))",
+                    "(c != d) | ((h(h(h(a))) = h(a)) | (f(b) = h(p(a,b))))",
+                    "(f(a) = c) | ((f(b) = f(f(g(h(d))))) | (a = c))",
+                    "(e = r) | (s = a)",
+                    "m(e, r) = m(a, b)",
+                    "f(a) != g(h(m(a, r)))",
+                    "m(e, r) = m(r, e)",
+                    "(g(h(d)) != f(b)) | (g(h(m(r, a))) = f(b))",
+                    "f(f(f(b))) = f(b)",
+                    "g(h(m(r, a))) != h(p(a, b))",
+                    "h(a) = e",
+                    "h(e) = r",
+                    "h(r) = b",
+                    "e != b",
+                    "p(a, b) = r",
+                    "f(b) != f(f(b))",
+                    "f(a) = c",
+                    "f(b) = d"]
 
 
 str_uf4 = "({0}) & (({1}) & (({2}) & (({3}) & (({4}) & (({5}) & (({6})" \
           " & (({7}) & (({8}) & (({9}) & (({10}) & (({11}) & (({12}) & " \
           "(({13}) & (({14}) & (({15}) &" \
-          " ((({16}) & ({17}))))))))))))))))))".format(*cls_strs_uf4)
+          " ((({16}) & ({17}))))))))))))))))))".format(*clauses_strs_uf4)
 
 
 uf_theory = UFTheory()
@@ -46,21 +49,49 @@ solver = DPLLT(uf_theory)
 parser = Parser()
 
 
-def test_uf1():
-    formula = parser.parse(str_uf1)
-    assert solver.solve(formula)[0] == ResultCode.UNSAT
+@pytest.mark.parametrize("formula_text, expected_result_code", [
+    (str_uf1, ResultCode.UNSAT),
+    (str_uf2, ResultCode.SAT),
+    (str_uf3, ResultCode.UNSAT),
+    (str_uf4, ResultCode.SAT)
+])
+def test_dpllt_with_uf(formula_text, expected_result_code):
+    formula = parser.parse(formula_text)
+    assert solver.solve(formula)[0] == expected_result_code
 
 
-def test_uf2():
-    formula = parser.parse(str_uf2)
-    assert solver.solve(formula)[0] == ResultCode.SAT
+def test_eqs_neqs_args_no_errors():
+    formula_text = "(a = b) & (y -> (x | (a != c)))"
+    formula = parser.parse(formula_text)
+    solver.solve(formula)
 
 
-def test_uf3():
-    formula = parser.parse(str_uf3)
-    assert solver.solve(formula)[0] == ResultCode.UNSAT
+@pytest.mark.parametrize("formula_text", [
+    "((a != b) & (f(x) != y)) | (a = (d | y))",
+    "(a != !b) & ((!!c) = d)",
+    "a = (a != b)"
+])
+def test_eqs_neqs_args_errors(formula_text):
+    with pytest.raises(ValueError):
+        formula = parser.parse(formula_text)
+        solver.solve(formula)
 
 
-def test_uf4():
-    formula = parser.parse(str_uf4)
-    assert solver.solve(formula)[0] == ResultCode.SAT
+@pytest.mark.parametrize("formula_text", [
+    "(a = b) & (f(c, d) = g(a))",
+    "f(c, !d)"
+])
+def test_func_args_no_errors(formula_text):
+    formula = parser.parse(formula_text)
+    solver.solve(formula)
+
+
+@pytest.mark.parametrize("formula_text", [
+    "f(c, d=c)",
+    "f(a, f(a, g(d), a | b), c)",
+    "f(a, !!c)"
+])
+def test_func_args_errors(formula_text):
+    with pytest.raises(ValueError):
+        formula = parser.parse(formula_text)
+        solver.solve(formula)
