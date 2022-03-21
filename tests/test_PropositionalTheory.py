6cbc7a84408281ea846db4b3996384651dd68224
Yuval <yuval,jacoby1@mail.huji.ac.il>
2022-03-21 12:50:50 +0200

    replacing PropositionalTheory tests with ones applicable for its preprocessing

---
+++ tests/test_PropositionalTheory.py
@@ -0,0 +1,21 @@
+import pytest
+from parsing.parse import Parser
+from theories.PropositionalTheory import PropositionalTheory
+
+theory = PropositionalTheory()
+parser = Parser()
+
+
+@pytest.mark.parametrize("formula_str", [
+    "[1, 2] < 3",
+    "f(a)",
+    "a & (f(a))",
+    "a = b",
+    "a != b",
+    "[1, 2] >= 3"
+], ids=repr)
+def test_atom_types_checks_errors(formula_str):
+    formula = parser.parse(formula_str)
+
+    with pytest.raises(ValueError):
+        theory.preprocess(formula)
